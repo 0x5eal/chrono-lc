ff1df090a1bc55574ffe2bb4c04014ba017c6f36
Erica Marigold <hi@devcomp.xyz>
2023-08-23 18:50:41 +0530

    chore: update installation rustdoc

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@
 //! ```toml
 //! [dependencies]
 //! chrono = "0.4"
-//! chrono_locale = "0.1"
+//! chrono_locale = { git = "https://github.com/0x5eal/chrono-locale.git", rev = "f8599bf" }
 //! ```
 //!
 //! Then put this in your `lib.rs` or `main.rs`:
